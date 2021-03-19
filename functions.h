0b23dc9a4b879bf2cb0e7479d6fc3a84546e73f7
Arthur Bringel <arthurbringel@gmail.com>
2021-03-18 23:46:48 -0300

    Final final

--- functions.h
+++ functions.h
@@ -10,7 +10,7 @@ void selecionar_dicionario(char *arq_dic, int *qtde_dic, int **p_contA, int **p_
 void selecionar_tra(char *arq_dic, char *arq_tra, int *contA);
 void selecionar_trb(char *arq_dic, char *arq_trb, int *contB);
 void exibir_tabela(char *arq_dic,int contA[], int contB[]);
-void exibir_bow_tr(int qtde_dic);
+void exibir_bow_tr(char* arq_dic,char* arq_tra,char* arq_trb);
 void dist_euclid(int contA[], int contB[], int qtde_dic);
 void flush_in();
 
